f177ccddb9855c3d0d1a5d9f58b10fc74eb718c6
Julia <jullien.fall@gmail.com>
2017-12-21 00:03:03 +0100

    done

--- js/main.js
+++ js/main.js
@@ -10,11 +10,13 @@ $(document).ready(function(){
             $('.navbar').addClass('fixed-top');
             $('.nav-link').addClass('nav-link-scroll');
             $('.navbar-brand').addClass('navbar-brand-scroll');
+            $('.navbar-toggler').addClass('navbar-toggler-scroll');
         } 
         else {
             $('.navbar').removeClass('nav-bar-scroll')
             $('.navbar').removeClass('fixed-top')
             $('.nav-link').removeClass('nav-link-scroll');
             $('.navbar-brand').removeClass('navbar-brand-scroll');
+            $('.navbar-toggler').removeClass('navbar-toggler-scroll');
         }
     });
