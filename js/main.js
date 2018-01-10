7a502dfcd9b1fc304120ad32c530846a525e5397
Julia <jullien.fall@gmail.com>
2018-01-10 14:34:08 +0100

    slider/form

--- js/main.js
+++ js/main.js
@@ -1,22 +1,17 @@
-$(document).ready(function(){
-   topScroll();
-
-    
+$(document).ready(function () {
+    topScroll();
 });
 
-    $(window).scroll(function topScroll(){
-        if ($(window).scrollTop() > $('.navbar').height()){
-            $('.navbar').addClass('nav-bar-scroll');
-            $('.navbar').addClass('fixed-top');
-            $('.nav-link').addClass('nav-link-scroll');
-            $('.navbar-brand').addClass('navbar-brand-scroll');
-            $('.navbar-toggler').addClass('navbar-toggler-scroll');
-        } 
-        else {
-            $('.navbar').removeClass('nav-bar-scroll')
-            $('.navbar').removeClass('fixed-top')
-            $('.nav-link').removeClass('nav-link-scroll');
-            $('.navbar-brand').removeClass('navbar-brand-scroll');
-            $('.navbar-toggler').removeClass('navbar-toggler-scroll');
-        }
-    });
+$(window).scroll(function topScroll() {
+    if ($(window).scrollTop() > $('.navbar').height()) {
+        $('.navbar').addClass('nav-bar-scroll');
+        $('.nav-link').addClass('nav-link-scroll');
+        $('.navbar-brand').addClass('navbar-brand-scroll');
+        $('.navbar-toggler').addClass('navbar-toggler-scroll');
+    } else {
+        $('.navbar').removeClass('nav-bar-scroll')
+        $('.nav-link').removeClass('nav-link-scroll');
+        $('.navbar-brand').removeClass('navbar-brand-scroll');
+        $('.navbar-toggler').removeClass('navbar-toggler-scroll');
+    }
+});
